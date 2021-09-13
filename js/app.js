7a3b83db474b1d387c531c9d0ffde2362386efa0
nhnajmul17 <nhnajmul17@gmail.com>
2021-09-13 12:04:32 +0600

    final

--- js/app.js
+++ js/app.js
@@ -20,8 +20,8 @@ const showProducts = (products) => {
       <p>Rating:<span class=text-danger> ${product.rating.rate}</span></p>
       <p>Rating Counts:<span class=text-success> ${product.rating.count}</span></p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success rounded-pill">add to cart</button>
+      <button id="details-btn" class="btn btn-danger rounded-pill">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
